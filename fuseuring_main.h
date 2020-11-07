ed847f5c5f38208986b70f08cdd5646d3d28da00
Martin Raiber <martin@urbackup.org>
2020-11-07 17:54:08 +0100

    Fix build

--- fuseuring_main.h
+++ fuseuring_main.h
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) Martin Raiber
 #pragma once
-#include "../Interface/File.h"
 #include <string>
 
 int fuseuring_main(int backing_fd, const std::string& mountpoint, int max_background, int congestion_threshold);
